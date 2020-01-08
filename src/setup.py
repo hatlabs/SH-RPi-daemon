2660589227f0415c2e2f81e42d5dd1aeb52e915e
Matti Airas <mairas@iki.fi>
2020-01-08 16:43:13 +0200

    Bump version, list modules explicitly

--- src/setup.py
+++ src/setup.py
@@ -1,17 +1,19 @@
 from setuptools import setup, find_packages
 
 setup(name='pi_super_ups',
-      version='0.1.0',
+      version='0.2.0',
       description='Super-capacitor UPS monitor for Raspberry Pi',
       author='Matti Airas',
       author_email='mairas@iki.fi',
       url='https://github.com/mairas/pi-super-ups',
       packages=find_packages(),
+      py_modules=['pi_super_ups.ups_hat_device'],
       install_requires=[
         'smbus2',
       ],
       entry_points = {
-        'console_scripts': ['pi-super-ups=pi_super_ups.pi_super_ups:main'],
-        'console_scripts': ['hat-i2c-report=pi_super_ups.hat_i2c_report:main'],
+        'console_scripts': [
+          'pi-super-ups=pi_super_ups.pi_super_ups:main',
+          'hat-i2c-report=pi_super_ups.hat_i2c_report:main'],
       }
 )
