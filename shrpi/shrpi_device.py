9defc33025466dc72ef17f04108cab905c45a05f
Matti Airas <mairas@iki.fi>
2022-12-09 20:21:33 +0200

    Update the protocol to support word-wide data

--- shrpi/shrpi_device.py
+++ shrpi/shrpi_device.py
@@ -2,9 +2,10 @@ from enum import Enum
 
 from smbus2 import SMBus
 
-VCAP_MAX = 2.75
-RATIO_COEFF = VCAP_MAX / 256
+VCAP_MAX = 9.35
 DCIN_MAX = 32.1
+I_MAX = 2.5
+TEMP_MAX = 512.
 
 class States(Enum):
     BEGIN = 0
@@ -21,12 +22,19 @@ class States(Enum):
     WATCHDOG_REBOOT = 11
     ENTER_OFF = 12
     OFF = 13
+    ENTER_SLEEP_SHUTDOWN = 14
+    SLEEP_SHUTDOWN = 15
+    ENTER_SLEEP = 16
+    SLEEP = 17
 
 
 class SHRPiDevice:
     def __init__(self, bus, addr):
         self.bus = bus
         self.addr = addr
+        self.hardware_version = None
+        self.firmware_version = None
+        self.read_analog = self.read_analog_byte  # default to v1 protocol
 
     def i2c_query_byte(self, reg):
         with SMBus(self.bus) as bus:
@@ -34,39 +42,100 @@ class SHRPiDevice:
             b = bus.read_byte(self.addr)
         return b
 
+    def i2c_query_bytes(self, reg, n):
+        with SMBus(self.bus) as bus:
+            bus.write_byte(self.addr, reg)
+            b = bus.read_i2c_block_data(self.addr, 0, n)
+        return b
+
+    def i2c_query_word(self, reg):
+        with SMBus(self.bus) as bus:
+            bus.write_byte(self.addr, reg)
+            w = bus.read_word_data(self.addr, 0)
+        return w
+
     def i2c_write_byte(self, reg, val):
         with SMBus(self.bus) as bus:
             bus.write_byte_data(self.addr, reg, val)
 
+    def set_hardware_version(self, version):
+        self.hardware_version = version
+
+    def set_firmware_version(self, version):
+        self.firmware_version = version
+        if version.startswith("2."):
+            self.read_analog = self.read_analog_word
+
+    def read_analog_byte(self, reg, scale):
+        return scale*self.i2c_query_byte(reg)/256
+
+    def read_analog_word(self, reg, scale):
+        return scale*self.i2c_query_word(reg)/65536
+
     def hardware_version(self):
-        return self.i2c_query_byte(0x01)
+        legacy_version = self.i2c_query_byte(0x01)
+        if legacy_version != 0xff:
+            version_string = f"1.0.{legacy_version}"
+        else:
+            bytes = self.i2c_query_bytes(0x03, 4)
+            version_string = f"{bytes[0]}.{bytes[1]}.{bytes[2]}"
+            if bytes[3] != 0xff:
+                version_string += f"-{bytes[3]}"
+        self.set_hardware_version(version_string)
+        return version_string
 
     def firmware_version(self):
-        return self.i2c_query_byte(0x02)
+        legacy_version = self.i2c_query_byte(0x02)
+        if legacy_version != 0xff:
+            version_string = f"1.0.{legacy_version}"
+        else:
+            bytes = self.i2c_query_bytes(0x04, 4)
+            version_string = f"{bytes[0]}.{bytes[1]}.{bytes[2]}"
+            if bytes[3] != 0xff:
+                version_string += f"-{bytes[3]}"
+        self.set_firmware_version(version_string)
+        return version_string
 
     def en5v_state(self):
         return self.i2c_query_byte(0x10)
 
+    def set_watchdog_timeout(self, timeout):
+        self.i2c_write_byte(0x12, int(10*timeout))
+
     def power_on_threshold(self):
-        return RATIO_COEFF * self.i2c_query_byte(0x13)
+        return self.read_analog(0x13, VCAP_MAX)
 
     def power_off_threshold(self):
-        return RATIO_COEFF * self.i2c_query_byte(0x14)
+        return self.read_analog(0x14, VCAP_MAX)
 
     def state(self):
         return States(self.i2c_query_byte(0x15)).name
 
     def dcin_voltage(self):
-        return DCIN_MAX*self.i2c_query_byte(0x20)/256
+        return self.read_analog(0x20, DCIN_MAX)
 
     def supercap_voltage(self):
-        return VCAP_MAX*self.i2c_query_byte(0x21)/256
+        return self.read_analog(0x21, VCAP_MAX)
+
+    def input_current(self):
+        if self.firmware_version.startswith("2."):
+            return self.read_analog(0x22, I_MAX)
+        else:
+            # input current measurement not supported in v1 hardware
+            return None
+
+    def temperature(self):
+        if self.firmware_version.startswith("2."):
+            return self.read_analog(0x23, TEMP_MAX)
+        else:
+            # temperature measurement not supported in v1 firmware
+            return None
 
     def request_shutdown(self):
         self.i2c_write_byte(0x30, 0x01)
 
-    def set_watchdog_timeout(self, timeout):
-        self.i2c_write_byte(0x12, int(10*timeout))
+    def request__sleep(self):
+        self.i2c_write_byte(0x31, 0x01)
 
     def watchdog_elapsed(self):
         return 0.1 * self.i2c_query_byte(0x16)
